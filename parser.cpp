a4720f583754fc3a007c2eee28e3cc773dd4aeb0
Jared <Jared@wlan-420-160-173-dhcp.csuchico.edu>
2013-04-11 11:22:19 -0700

    Fixes right most issue and Adds Epsilons
    Fixes an issue where the right most non-terminal was being
    evaluated first. Now it starts from the left most non-terminal
    to apply the grammar rules.
    
    Adds epsilon to the grammar. This brought forth a tricky problem
    because you can replicate non-terminals forever and never find the
    solution, so I had to implement a check to prevent the state from
    falling into an infinite loop. If the size of the current state
    is more than 10 more the size of the final state, then skip applying
    rules.

--- parser.cpp
+++ parser.cpp
@@ -16,9 +16,9 @@ using namespace std;
 typedef struct node {
   node(string state, node *parent)
     :m_state(state),m_parent(parent){}
-	bool m_visited;
-	string m_state;
-	node* m_parent;
+  bool m_visited;
+  string m_state;
+  node* m_parent;
 };
 list<node *> states;
 int main()
@@ -30,35 +30,33 @@ int main()
 // Constraints: 1. non-terminals must be A-Z
 //              2. terminals must be a-z
 //              3. Start state must be S
+//              4. & equals epsilon
 //         
 ///////////////////////////////////////////////////////////////////
-  string finalState = "aaab";
 
-  // S -> AA | A | B
-  rules['S'].push_back("AA");
-  rules['S'].push_back("A");
-  rules['S'].push_back("B");
+  string finalState = "baabbb";
 
-  // A -> AaA | a | b
-  rules['A'].push_back("AaA");
-  rules['A'].push_back("a");
-  rules['A'].push_back("b");
+  // S -> aAa | bAb | epsilon
+  rules['S'].push_back("aAa");
+  rules['S'].push_back("bAb");
+  rules['S'].push_back("&");
 
-  // B -> b | a | A
-  rules['B'].push_back("b");
-  rules['B'].push_back("a");
-  rules['B'].push_back("A");
-
+  // A -> SS
+  rules['A'].push_back("SS");
+  
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
   bool foundSolution = false;
   states.push_back(new node("S", NULL));
   while(!states.empty()){
-  	node* cur = states.front();
-  	states.pop_front();
+    node* cur = states.front();
+    states.pop_front();
+    //cout << cur->m_state << endl;
     // No way we can match if our current state is larger than our final state
-    if(cur->m_state.size() > finalState.size()) continue;
+    // Update: If we're including epsilons, we might have a string larger than our final state
+    //         Add 10 as an offset to catch most cases.
+    if(cur->m_state.size() > finalState.size()+10) continue;
     // Check if our state is the final state
     if(finalState.compare(cur->m_state) == 0){
       string output = finalState;
@@ -76,12 +74,16 @@ int main()
       if(cur->m_state[i] >= 'A' && cur->m_state[i] <= 'Z'){
         non_terminal = cur->m_state[i];
         non_terminal_index = i;
+        break; // use the first occurence
       }
     }
     // Apply all rules and push new states onto the queue
     if(non_terminal - '0')
       for(int sz=rules[non_terminal].size(),i=0;i < sz;++i){
-        nodesToFree.push_back(new node(string(cur->m_state).replace(non_terminal_index,1,rules[non_terminal][i]),cur));
+        if(rules[non_terminal][i][0] == '&')
+          nodesToFree.push_back(new node(string(cur->m_state).erase(non_terminal_index,1),cur));
+        else
+          nodesToFree.push_back(new node(string(cur->m_state).replace(non_terminal_index,1,rules[non_terminal][i]),cur));
         states.push_back(nodesToFree[nodesToFree.size()-1]);
       }
   }
